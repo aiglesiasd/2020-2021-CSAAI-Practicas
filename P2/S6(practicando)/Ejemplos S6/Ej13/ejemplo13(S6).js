893241e55552b9be5d22b03de9f3986400bf21c8
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-04-11 18:32:42 +0200

    ejemplo 13(S6) practicando

---
+++ P2/S6(practicando)/Ejemplos S6/Ej13/ejemplo13(S6).js
@@ -0,0 +1,64 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas(horizontal=600,vertical=200)
+canvas.width = 600;
+canvas.height = 300
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
+
+//-- Velocidad horizontal del objeto
+let velx = 4;
+let vely = 0.2;
+//-- Funcion principal de animacion
+function update() 
+{
+  console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+
+  //-- Comprobar colisión con borde derecho
+  //--lo que le estoy poniendo es que si alcanza la altura del canvas 0 x es menor que 0, que invierta la velocidad
+  //-- 
+  if (x < 0 || x >= (canvas.width - 20) ) {
+    velx = -velx;
+  }
+  //para que rebote en la horizontal de arriba y abajo:
+   //-- Condición de rebote en extremos horizontales del canvas
+   if (y <= 0 || y > 280) {
+    vely = -vely;
+  }
+  //-- se actualiza la posicion asi
+  x = x + velx;
+  y = y + vely;
+
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
