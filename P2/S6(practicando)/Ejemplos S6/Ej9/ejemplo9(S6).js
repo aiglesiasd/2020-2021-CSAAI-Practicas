f5955556fb5024f55770c36e7b3f9342b77b0f17
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-04-11 17:52:19 +0200

    ejemplo 9 practicando

---
+++ P2/S6(practicando)/Ejemplos S6/Ej9/ejemplo9(S6).js
@@ -0,0 +1,50 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas
+canvas.width = 600;
+canvas.height = 300;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
+
+//-- Velocidad horizontal del objeto
+let velx = 1;
+
+//-- Funcion principal de animacion
+function update() 
+{
+  console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+  x = x + velx;
+
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 40, 40,);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
