8fec8797bf618860bab2eba503cf62eb74966e69
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-04-29 13:22:31 +0200

    bucle para botones y operadores y definir arrays

--- P2/Calculadora/calculadora.js
+++ P2/Calculadora/calculadora.js
@@ -5,9 +5,10 @@ suma = document.getElementById("suma");
 igual = document.getElementById("igual");
 AC = document.getElementById("AC"); //el AC es borrar todo
 DEL = document.getElementById("DEL"); //el DEL es borrar un numeross
-
-let botones = document.getElementById("boton");
-let operaciones = document.getElementById("operaciones");
+//los botones que representan números los podemos agrupar en la clase botones
+let boton = document.getElementsByClassName("botones");
+//...clase operaciones
+let operacion = document.getElementByClassName("operaciones");
 //-- Estados de la calculadora
 const ESTADO = {
     INIT: 0,
@@ -15,10 +16,22 @@ const ESTADO = {
     OPERATION: 2,
     OP2: 3
 }
+//hacemos nuestro bucle para los botones
+for(i=0;i<boton.length;i++){
+    boton[i].onclick=(ev)=>{
+        botones(ev.target.value) //El parámetro ev.target contiene el boton que ha recibido el click   
+    
+    }
+}
+//hacemos nuestro bucle para los operadores
+for(i=0;i<operacion.length;i++){
+    operacion[i].onclick=(ev)=>{
+        operaciones(ev.target.value)    
+    }
+}
  
- 
- //Empezamos en el estado inicial de nuestra maquina de estados
- let estado = ESTADO.INIT;   
+//Empezamos en el estado inicial de nuestra maquina de estados
+let estado = ESTADO.INIT;   
 
 //actualizamos nuestro display a cero cuando clickeamos el boton AC...
 //.. y volvemos al estado inicial(INIT)
