795d3cbdc37dd04e0dcc217f30abfc9dd040e67f
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-05-30 13:46:28 +0200

    PRIMEROS CAMBIOS EN aire.js

---
+++ P4/aire.js
@@ -0,0 +1,8 @@
+console.log('Ejecutando JS...');
+
+//-- Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+var img = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+const Grises = document.getElementById("Grises");
+const Colores = document.getElementById("Colores");
