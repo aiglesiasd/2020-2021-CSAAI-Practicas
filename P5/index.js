640ed2fabe5e590a4e407235d7116a001be0a50a
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-05-30 20:48:26 +0200

    ultimos cambios

--- P5/index.js
+++ P5/index.js
@@ -8,13 +8,14 @@ const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
 const btn_src_off = document.getElementById("btn_src_off");
 
+
 //-- Establecer las dimensiones de los vídeos
 directo.width=500;
 directo.height=500;
 video1.width=200;  
-video1.height=100;
+video1.height=200;
 video2.width=200;
-video2.height=100;
+video2.height=200;
 
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "imagen.jpg";
@@ -24,41 +25,54 @@ const TEST_IMAGE_URL = "imagen.jpg";
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
+
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
  
   //-- Establecer la fuente de la cámara 1
-  video1.src="https://drive.google.com/file/d/1H5_ofaxKcuE13lvWaQGB7W6NUH-QlnyE/view?usp=sharing";
-  video2.src="https://drive.google.com/file/d/1-8rfn4tWIsjrV4cvPq00r5rlkpH4vty8/view?usp=sharing";
+  video1.src="https://drive.google.com/uc?export=download&id=1H5_ofaxKcuE13lvWaQGB7W6NUH-QlnyE";
+  video2.src="https://drive.google.com/uc?export=download&id=1-8rfn4tWIsjrV4cvPq00r5rlkpH4vty8";
+
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
   video2.currentTime = 0;
   video2.play();
+
   //-- Y en silencio...
   video1.muted = true;
   video2.muted = true;
+
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
 };
-
+//Boton de FUENTES-OFF
+btn_src_off.onclick = () => {
+    directo.poster = TEST_IMAGE_URL;
+    video1.poster = TEST_IMAGE_URL;
+    video2.poster = TEST_IMAGE_URL;
+    directo.src = null;
+    video1.src = null;
+    video2.src = null;
+}
 //-- Botón de Test
 btn_test.onclick = () => {
-    directo.poster = TEST_IMAGE_URL;
-    directo.src = null;
+directo.poster = TEST_IMAGE_URL;
+directo.src = null;
 };
 
 //-- Botón de Selección de la cámara 1
 btn_video1.onclick = () => {
-    directo.src = video1.src;
-    directo.currentTime = video1.currentTime;
-    directo.play();
-    directo.poster=null;
+directo.src = video1.src;
+directo.currentTime = video1.currentTime;
+directo.play();
+directo.poster=null;
 };
+
 //-- Botón de Selección de la cámara 2
 btn_video2.onclick = () => {
-    directo.src = video2.src;
-    directo.currentTime = video2.currentTime;
-    directo.play();
-    directo.poster=null;
+directo.src = video2.src;
+directo.currentTime = video2.currentTime;
+directo.play();
+directo.poster=null;
 };
