298abc868356c308eca63fefb26d7371cd6f6302
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-05-30 14:41:54 +0200

    acabo de añadir una funcion para el color..+

--- P4/aire.js
+++ P4/aire.js
@@ -8,13 +8,13 @@ const Selec1 = document.getElementById("Selec1");
 const Grises = document.getElementById("Grises");
 const Colores = document.getElementById("Colores");
 //-- Para los deslizadores
-const deslizador_R = document.getElementById('deslizador_rojo');
-const deslizador_G = document.getElementById('deslizador_verde');
-const deslizador_B = document.getElementById('deslizador_azul');
+const deslizador_rojo = document.getElementById('deslizador_rojo');
+const deslizador_verde = document.getElementById('deslizador_verde');
+const deslizador_azul = document.getElementById('deslizador_azul');
 //-- Para el valor de cada deslizador
-const range_value_R = document.getElementById('value_r');
-const range_value_G = document.getElementById('value_v');
-const range_value_B = document.getElementById('value_a');
+const value_r = document.getElementById('value_r');
+const value_v= document.getElementById('value_v');
+const value_a = document.getElementById('value_a');
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -39,3 +39,36 @@ Selec1.onclick = () => {
     imagen = document.getElementById('imagen');
     ctx.drawImage(imagen, 0, 0, canvas.width, canvas.height);
   }
+  Colores.onclick = () => {
+    estado = "Colores";
+    ctx.putImageData(paraFiltroColor(), 0, 0);
+  
+  }
+  
+  function paraFiltroColor() {
+    //-- Mostrar el nuevo valor del deslizador
+    value_r.innerHTML = deslizador_rojo.value;
+    value_v.innerHTML = deslizador_verde.value;
+    value_a.innerHTML = deslizador_azul.value;
+
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data;
+  
+    
+    rojo = deslizador_rojo.value;
+    verde = deslizador_verde.value;
+    azul = deslizador_azul.value;
+    //-- Filtrar la imagen según el nuevo umbral
+    for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > rojo)
+        data[i] = rojo;
+      if (data[i+1] > verde)
+        data[i+1] = verde;
+      if (data[i+2] > azul)
+        data[i+2] = azul;
+    }
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+    return imgData;
+  }
+    
