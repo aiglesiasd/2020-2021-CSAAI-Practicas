a0da1f8455a7acd5a720d03be0df8b26793e4380
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-05-30 14:12:22 +0200

    cambiando cosas de mi imagen

--- P4/aire.js
+++ P4/aire.js
@@ -2,7 +2,7 @@ console.log('Ejecutando JS...');
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-var img = document.getElementById('imagen');
+var imagen = document.getElementById('imagen');
 const ctx = canvas.getContext('2d');
 const Grises = document.getElementById("Grises");
 const Colores = document.getElementById("Colores");
@@ -14,3 +14,27 @@ const deslizador_B = document.getElementById('deslizador_azul');
 const range_value_R = document.getElementById('value_r');
 const range_value_G = document.getElementById('value_v');
 const range_value_B = document.getElementById('value_a');
+
+//-- Función de retrollamada de imagen cargada
+//-- La imagen no se carga instantaneamente, sino que
+//-- lleva un tiempo. Sólo podemos acceder a ella una vez
+//-- que esté totalmente cargada
+imagen.onload = function () {
+
+    //-- Se establece como tamaño del canvas el mismo
+    //-- que el de la imagen original
+    canvas.width = imagen.width;
+    canvas.height = imagen.height;
+  
+    //-- Situar la imagen original en el canvas
+    ctx.drawImage(imagen, 0, 0, canvas.width, canvas.height);
+  
+    console.log("Imagen lista...");
+  
+  };
+  
+// Para la foto que quiero modificar cuando haga click..(seleccionamos)
+Sel1.onclick = () => {
+    imagen = document.getElementById('imagen');
+    ctx.drawImage(imagen, 0, 0, canvas.width, canvas.height);
+  }
