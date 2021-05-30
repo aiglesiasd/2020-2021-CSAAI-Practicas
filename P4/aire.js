e5f120ebf4f942bc6ae255f22e24a9299b45c5d1
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-05-30 13:55:23 +0200

    ordenando cosas para empezar con las funciones

--- P4/aire.js
+++ P4/aire.js
@@ -2,7 +2,15 @@ console.log('Ejecutando JS...');
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-var img = document.getElementById('imagesrc');
+var img = document.getElementById('imagen');
 const ctx = canvas.getContext('2d');
 const Grises = document.getElementById("Grises");
 const Colores = document.getElementById("Colores");
+//-- Para los deslizadores
+const deslizador_R = document.getElementById('deslizador_rojo');
+const deslizador_G = document.getElementById('deslizador_verde');
+const deslizador_B = document.getElementById('deslizador_azul');
+//-- Para el valor de cada deslizador
+const range_value_R = document.getElementById('value_r');
+const range_value_G = document.getElementById('value_v');
+const range_value_B = document.getElementById('value_a');
