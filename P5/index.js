7fada0355548e722cb18d181a2d4794eef29e01e
aiglesiasd <a.iglesiasd.2018@alumnos.urjc.es>
2021-05-30 20:07:49 +0200

    ahora hacemos lo mismo del video 1 para el video 2

--- P5/index.js
+++ P5/index.js
@@ -9,21 +9,21 @@ const btn_src_on = document.getElementById("btn_src_on");
 const btn_src_off = document.getElementById("btn_src_off");
 
 //-- Establecer las dimensiones de los vídeos
-directo.width=420;
-directo.height=200;
+directo.width=500;
+directo.height=500;
 video1.width=200;  
 video1.height=100;
 video2.width=200;
 video2.height=100;
 
 //-- Imagen de Test usada
-const TEST_IMAGE_URL = "imagen.png";
+const TEST_IMAGE_URL = "imagen.jpg";
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
-
+video2.poster = TEST_IMAGE_URL;
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
  
@@ -33,10 +33,11 @@ btn_src_on.onclick = () => {
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
-
+  video2.currentTime = 0;
+  video2.play();
   //-- Y en silencio...
-  video1.muted;
-
+  video1.muted = true;
+  video2.muted = true;
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
 };
@@ -51,6 +52,13 @@ btn_test.onclick = () => {
 btn_video1.onclick = () => {
     directo.src = video1.src;
     directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+//-- Botón de Selección de la cámara 2
+btn_video2.onclick = () => {
+    directo.src = video2.src;
+    directo.currentTime = video2.currentTime;
     directo.play();
     directo.poster=null;
 };
